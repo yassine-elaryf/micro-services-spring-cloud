a4f3c8f3f94e9daad35b9354c3247b5ab297c29d
Yassine El Aryf <73699118+yassine-elaryf@users.noreply.github.com>
2022-03-14 11:31:18 +0100

    Add files via upload

---
+++ service-company/src/main/java/com/example/demo/dao/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.demo.entities.Company;
+
+@RepositoryRestResource
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+}
